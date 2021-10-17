7f09498aaf3d6e92562294a2be84daa10984d895
Uziel Gaeta <ugaeta@cpp.edu>
2021-10-16 20:31:48 -0700

    started to implement solvePostFix

--- ArrayStackTest.java
+++ ArrayStackTest.java
@@ -1,3 +1,13 @@
 public class ArrayStackTest {
-    
+    public static void main(String[] args) {
+        String postFix = "ab*ca-/de*+";
+        int result = solvePostFix(postFix);
+
+        System.out.printf("The solution to '%s' is: %d\n", postFix, result);
+    }
+
+    private static int solvePostFix(String postFix){
+        int solution = 0;
+        return solution;
+    }
 }
